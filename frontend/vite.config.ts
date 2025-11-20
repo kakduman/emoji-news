a8edd7288544366555c8bd659b58b792738b6211
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-11-19 20:58:09 -0800

    Update vite base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base =
-  process.env.GITHUB_REPOSITORY !== undefined
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base,
+  base: '/',
   plugins: [react()],
 })
